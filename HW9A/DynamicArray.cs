2b04d9a10274c8144d34011f2ee4d958037a4f8b
Dmytro Stoliar <drleavsy@gmail.com>
2016-12-19 20:14:15 +0200

    corrected/implemented Dynamic Array class

--- HW9A/DynamicArray.cs
+++ HW9A/DynamicArray.cs
@@ -6,26 +6,235 @@ using System.Threading.Tasks;
 
 namespace HW9A
 {
-    abstract class DynamicArray<T> 
+    class DynamicArray<T> : DynamicArrayAbstract<T>
     {
-        protected int sizeOfDynamicArr;
-        protected int capacityOfDynamicArr;
-        protected T [] DynamicArr;
-        protected int MaxSize;
-        protected T TValue; //
-        protected int count; // 
 
-        abstract protected void Add(T newAdd);
-        abstract protected void IncreaseSize();
-        abstract protected void DecreaseSize();
-        abstract protected void Insert(int inx,T newAdd);
-        abstract protected T Get(int inx);
-        abstract protected T Remove(int inx);
-        abstract protected int Size();
+        protected override void Add(T newAdd)
+        {
+            if (sizeOfDynamicArr > 0 && sizeOfDynamicArr < capacityOfDynamicArr)
+            {
+                DynamicArr[sizeOfDynamicArr] = newAdd;
+                sizeOfDynamicArr++;
+            }
+            else if (sizeOfDynamicArr > 0 && sizeOfDynamicArr >= capacityOfDynamicArr)
+            {
+                IncreaseSize();
+                DynamicArr[sizeOfDynamicArr] = newAdd;
+                sizeOfDynamicArr++;
+            }
+            else if (sizeOfDynamicArr == 0)
+            {
+                IncreaseSize();
+                DynamicArr[sizeOfDynamicArr] = newAdd;
+                sizeOfDynamicArr++;
+            }
+        }
 
-        public abstract bool IsFull();
-        public abstract bool IsEmpty();
-        public abstract T Peek();
-        public abstract void Print();
+        protected override void Insert(int inx, T newAdd)
+        {
+            if (sizeOfDynamicArr < capacityOfDynamicArr && inx < sizeOfDynamicArr && inx >= 0)
+            {
+                T[] DynamicArrLower = new T[inx];
+                int j = 0;
+                for (int i = 0; i < inx; i++)
+                {
+                    DynamicArrLower[j] = DynamicArr[i];
+                    j++;
+                }
+
+                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx];
+                int k = 0;
+                for (int i = inx; i < sizeOfDynamicArr; i++)
+                {
+                    DynamicArrUpper[k] = DynamicArr[i];
+                    k++;
+                }
+                int m = 0;
+                int n = 0;
+                for (int i = 0; i < j; i++)
+                {
+                    DynamicArr[i] = DynamicArrLower[m];
+                    m++;
+                }
+                DynamicArr[m] = newAdd;
+                for (int i = j + 1; i < k + j + 1; i++)
+                {
+                    DynamicArr[i] = DynamicArrUpper[n];
+                    n++;
+                }
+                sizeOfDynamicArr++;
+            }
+            else
+            {
+                IncreaseSize();
+
+                T[] DynamicArrLower = new T[inx];
+                int j = 0;
+                for (int i = 0; i < inx; i++)
+                {
+                    DynamicArrLower[j] = DynamicArr[i];
+                    j++;
+                }
+
+                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx];
+                int k = 0;
+                for (int i = inx; i < sizeOfDynamicArr; i++)
+                {
+                    DynamicArrUpper[k] = DynamicArr[i];
+                    k++;
+                }
+                int m = 0;
+                int n = 0;
+                for (int i = 0; i < j; i++)
+                {
+                    DynamicArr[i] = DynamicArrLower[m];
+                    m++;
+                }
+                DynamicArr[m] = newAdd;
+                for (int i = j + 1; i < k + j + 1; i++)
+                {
+                    DynamicArr[i] = DynamicArrUpper[n];
+                    n++;
+                }
+                sizeOfDynamicArr++;
+            }
+        }
+
+        protected override T Get(int inx)
+        {
+            if (inx >= 0 && inx < sizeOfDynamicArr)
+            {
+                return DynamicArr[inx];
+            }
+            return TValue;
+        }
+
+        protected override T Remove(int inx)
+        {
+            if (sizeOfDynamicArr <= capacityOfDynamicArr / 2 && inx < sizeOfDynamicArr && inx >= 0)
+            {
+                DecreaseSize();
+
+                T[] DynamicArrLower = new T[inx];
+                int j = 0;
+                for (int i = 0; i < inx; i++)
+                {
+                    DynamicArrLower[j] = DynamicArr[i];
+                    j++;
+                }
+                TValue = DynamicArr[inx]; // value to remove
+
+                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx - 1];
+                int k = 0;
+                for (int i = inx + 1; i < sizeOfDynamicArr; i++)
+                {
+                    DynamicArrUpper[k] = DynamicArr[i];
+                    k++;
+                }
+                int m = 0;
+                int n = 0;
+                for (int i = 0; i < j - 1; i++)
+                {
+                    DynamicArr[i] = DynamicArrLower[m];
+                    m++;
+                }
+                for (int i = j; i < j + k; i++)
+                {
+                    DynamicArr[i] = DynamicArrUpper[n];
+                    n++;
+                }
+                sizeOfDynamicArr--;
+                if (sizeOfDynamicArr == 0)
+                {
+                    DynamicArr = null;
+                    capacityOfDynamicArr = 0;
+                }
+                return TValue;
+            }
+            else if (inx < sizeOfDynamicArr && inx >= 0)
+            {
+                T[] DynamicArrLower = new T[inx];
+                int j = 0;
+                for (int i = 0; i < inx; i++)
+                {
+                    DynamicArrLower[j] = DynamicArr[i];
+                    j++;
+                }
+                TValue = DynamicArr[inx]; // value ot remove
+
+                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx];
+                int k = 0;
+                for (int i = inx + 1; i < sizeOfDynamicArr; i++)
+                {
+                    DynamicArrUpper[k] = DynamicArr[i];
+                    k++;
+                }
+                int m = 0;
+                int n = 0;
+                for (int i = 0; i < j; i++)
+                {
+                    DynamicArr[i] = DynamicArrLower[m];
+                    m++;
+                }
+                for (int i = j; i < j + k; i++)
+                {
+                    DynamicArr[i] = DynamicArrUpper[n];
+                    n++;
+                }
+                sizeOfDynamicArr--;
+                if (sizeOfDynamicArr == 0)
+                {
+                    DynamicArr = null;
+                    capacityOfDynamicArr = 0;
+                }
+                return TValue;
+            }
+            return TValue;
+        }
+        protected override void IncreaseSize()
+        {
+            if (sizeOfDynamicArr >= capacityOfDynamicArr && sizeOfDynamicArr > 0)
+            {
+                capacityOfDynamicArr = sizeOfDynamicArr + sizeOfDynamicArr;
+                T[] TempDynamicArr = new T[capacityOfDynamicArr];
+
+                for (int i = 0; i < sizeOfDynamicArr; i++)
+                {
+                    TempDynamicArr[i] = DynamicArr[i];
+                }
+
+                DynamicArr = new T[capacityOfDynamicArr];
+                for (int i = 0; i < sizeOfDynamicArr; i++)
+                {
+                    DynamicArr[i] = TempDynamicArr[i];
+                }
+            }
+            else if (sizeOfDynamicArr == 0)
+            {
+                capacityOfDynamicArr = 2;
+                sizeOfDynamicArr = 0;
+                DynamicArr = new T[capacityOfDynamicArr];
+            }
+        }
+        protected override void DecreaseSize()
+        {
+            if (sizeOfDynamicArr <= capacityOfDynamicArr / 2 && sizeOfDynamicArr >= 0)
+            {
+                capacityOfDynamicArr = capacityOfDynamicArr / 2;
+                T[] TempDynamicArr = new T[sizeOfDynamicArr];
+
+                for (int i = 0; i < sizeOfDynamicArr; i++)
+                {
+                    TempDynamicArr[i] = DynamicArr[i];
+                }
+
+                DynamicArr = new T[capacityOfDynamicArr];
+                for (int i = 0; i < sizeOfDynamicArr; i++)
+                {
+                    DynamicArr[i] = TempDynamicArr[i];
+                }
+                sizeOfDynamicArr = capacityOfDynamicArr;
+            }
+        }
     }
 }
--- HW9A/DynamicQueue.cs
+++ HW9A/DynamicQueue.cs
@@ -15,240 +15,7 @@ namespace HW9A
             MaxSize = maxSize;
         }
 
-        protected override void Add(T newAdd)
-        {
-            if (sizeOfDynamicArr > 0 && sizeOfDynamicArr < capacityOfDynamicArr)
-            {
-                DynamicArr[sizeOfDynamicArr] = newAdd;
-                sizeOfDynamicArr++;
-            }
-            else if (sizeOfDynamicArr > 0 && sizeOfDynamicArr >= capacityOfDynamicArr)
-            {
-                IncreaseSize();
-                DynamicArr[sizeOfDynamicArr] = newAdd;
-                sizeOfDynamicArr++;
-            }
-            else if (sizeOfDynamicArr == 0)
-            {
-                IncreaseSize();
-                DynamicArr[sizeOfDynamicArr] = newAdd;
-                sizeOfDynamicArr++;
-            }
-        }
-
-        protected override void Insert(int inx, T newAdd)
-        {
-            if (sizeOfDynamicArr < capacityOfDynamicArr && inx < sizeOfDynamicArr && inx >= 0)
-            {
-                T[] DynamicArrLower = new T[inx];
-                int j = 0;
-                for (int i = 0; i < inx; i++)
-                {
-                    DynamicArrLower[j] = DynamicArr[i];
-                    j++;
-                }
-
-                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx];
-                int k = 0;
-                for (int i = inx; i < sizeOfDynamicArr; i++)
-                {
-                    DynamicArrUpper[k] = DynamicArr[i];
-                    k++;
-                }
-                int m = 0;
-                int n = 0;
-                for (int i = 0; i < j; i++)
-                {
-                    DynamicArr[i] = DynamicArrLower[m];
-                    m++;
-                }
-                DynamicArr[m] = newAdd;
-                for (int i = j + 1; i < k + j + 1; i++)
-                {
-                    DynamicArr[i] = DynamicArrUpper[n];
-                    n++;
-                }
-                sizeOfDynamicArr++;
-            }
-            else
-            {
-                IncreaseSize();
-
-                T[] DynamicArrLower = new T[inx];
-                int j = 0;
-                for (int i = 0; i < inx; i++)
-                {
-                    DynamicArrLower[j] = DynamicArr[i];
-                    j++;
-                }
-
-                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx];
-                int k = 0;
-                for (int i = inx; i < sizeOfDynamicArr; i++)
-                {
-                    DynamicArrUpper[k] = DynamicArr[i];
-                    k++;
-                }
-                int m = 0;
-                int n = 0;
-                for (int i = 0; i < j; i++)
-                {
-                    DynamicArr[i] = DynamicArrLower[m];
-                    m++;
-                }
-                DynamicArr[m] = newAdd;
-                for (int i = j + 1; i < k + j + 1; i++)
-                {
-                    DynamicArr[i] = DynamicArrUpper[n];
-                    n++;
-                }
-                sizeOfDynamicArr++;
-            }
-        }
-
-        protected override T Get(int inx)
-        {
-            if (inx >= 0 && inx < sizeOfDynamicArr)
-            {
-                return DynamicArr[inx];
-            }
-            return TValue;
-        }
-
-        protected override T Remove(int inx)
-        {
-            if (sizeOfDynamicArr <= capacityOfDynamicArr / 2 && inx < sizeOfDynamicArr && inx >= 0)
-            {
-                DecreaseSize();
-
-                T[] DynamicArrLower = new T[inx];
-                int j = 0;
-                for (int i = 0; i < inx; i++)
-                {
-                    DynamicArrLower[j] = DynamicArr[i];
-                    j++;
-                }
-                TValue = DynamicArr[inx]; // value to remove
-
-                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx - 1];
-                int k = 0;
-                for (int i = inx + 1; i < sizeOfDynamicArr; i++)
-                {
-                    DynamicArrUpper[k] = DynamicArr[i];
-                    k++;
-                }
-                int m = 0;
-                int n = 0;
-                for (int i = 0; i < j - 1; i++)
-                {
-                    DynamicArr[i] = DynamicArrLower[m];
-                    m++;
-                }
-                for (int i = j; i < j + k; i++)
-                {
-                    DynamicArr[i] = DynamicArrUpper[n];
-                    n++;
-                }
-                sizeOfDynamicArr--;
-                if (sizeOfDynamicArr == 0)
-                {
-                    DynamicArr = null;
-                    capacityOfDynamicArr = 0;
-                }
-                return TValue;
-            }
-            else if (inx < sizeOfDynamicArr && inx >= 0)
-            {
-                T[] DynamicArrLower = new T[inx];
-                int j = 0;
-                for (int i = 0; i < inx; i++)
-                {
-                    DynamicArrLower[j] = DynamicArr[i];
-                    j++;
-                }
-                TValue = DynamicArr[inx]; // value ot remove
-
-                T[] DynamicArrUpper = new T[sizeOfDynamicArr - inx];
-                int k = 0;
-                for (int i = inx + 1; i < sizeOfDynamicArr; i++)
-                {
-                    DynamicArrUpper[k] = DynamicArr[i];
-                    k++;
-                }
-                int m = 0;
-                int n = 0;
-                for (int i = 0; i < j; i++)
-                {
-                    DynamicArr[i] = DynamicArrLower[m];
-                    m++;
-                }
-                for (int i = j; i < j + k; i++)
-                {
-                    DynamicArr[i] = DynamicArrUpper[n];
-                    n++;
-                }
-                sizeOfDynamicArr--;
-                if (sizeOfDynamicArr == 0)
-                {
-                    DynamicArr = null;
-                    capacityOfDynamicArr = 0;
-                }
-                return TValue;
-            }
-            return TValue;
-        }
-        protected override void IncreaseSize()
-        {
-            if (sizeOfDynamicArr >= capacityOfDynamicArr && sizeOfDynamicArr > 0)
-            {
-                capacityOfDynamicArr = sizeOfDynamicArr + sizeOfDynamicArr;
-                T[] TempDynamicArr = new T[capacityOfDynamicArr];
-
-                for (int i = 0; i < sizeOfDynamicArr; i++)
-                {
-                    TempDynamicArr[i] = DynamicArr[i];
-                }
-
-                DynamicArr = new T[capacityOfDynamicArr];
-                for (int i = 0; i < sizeOfDynamicArr; i++)
-                {
-                    DynamicArr[i] = TempDynamicArr[i];
-                }
-            }
-            else if (sizeOfDynamicArr == 0)
-            {
-                capacityOfDynamicArr = 2;
-                sizeOfDynamicArr = 0;
-                DynamicArr = new T[capacityOfDynamicArr];
-            }
-        }
-        protected override void DecreaseSize()
-        {
-            if (sizeOfDynamicArr <= capacityOfDynamicArr / 2 && sizeOfDynamicArr >= 0)
-            {
-                capacityOfDynamicArr = capacityOfDynamicArr / 2;
-                T[] TempDynamicArr = new T[sizeOfDynamicArr];
-
-                for (int i = 0; i < sizeOfDynamicArr; i++)
-                {
-                    TempDynamicArr[i] = DynamicArr[i];
-                }
-
-                DynamicArr = new T[capacityOfDynamicArr];
-                for (int i = 0; i < sizeOfDynamicArr; i++)
-                {
-                    DynamicArr[i] = TempDynamicArr[i];
-                }
-                sizeOfDynamicArr = capacityOfDynamicArr;
-            }
-        }
-
-        protected override int Size()
-        {
-            return sizeOfDynamicArr;
-        }
-
-        public override bool IsFull()
+        public bool IsFull()
         {
             if (sizeOfDynamicArr >= MaxSize) //if stack is full
             {
@@ -261,7 +28,7 @@ namespace HW9A
             }
         }
 
-        public override bool IsEmpty()
+        public bool IsEmpty()
         {
             if (sizeOfDynamicArr == 0) // check if stack is not empty already
             {
@@ -274,12 +41,12 @@ namespace HW9A
             }
         }
 
-        public override T Peek()
+        public  T Peek()
         {
             return Get(sizeOfDynamicArr);
         }
 
-        public override void Print()
+        public  void Print()
         {
             int i = 0;
             int count_print = sizeOfDynamicArr;
